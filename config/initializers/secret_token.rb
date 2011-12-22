27ea1fbe024fd215c5657404bb070e1bc6a542af
gpl <gplomb@gplombproductions.com>
2011-12-22 00:16:19 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '57d4c9e4dfa47299d9fada5f974736dcfcef0d612e6c68ef6892cb53076137a0a08c26aaffd76c1035bdf73ed04646838a280403eaee0d646ec5925c8f41694a'
